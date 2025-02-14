1851bf968c46f79ffd2eb351250232ca6e1fd3b2
kaiksito <kaiksito@gmail.com>
2025-02-14 10:21:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("yesBtn").addEventListener("click", function() {
+    document.getElementById("response").textContent = "TE AMO CAPPUCHINO🤎🤍! 😍";
+  });
+  
+  document.getElementById("noBtn").addEventListener("click", function() {
+    document.getElementById("response").textContent = "QUE.NO ERES MIO PARA SIEMPRE 😡";
+  });
+  
